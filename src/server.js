10284df6a2f12402ea5a7ca6e72f623503fa917d
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-19 15:18:47 +0900

    socketIO설치 및 세팅

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import http from "http";
-import websocket from "ws";
+import socketIO from "socket.io";
 
 const app = express();
 
@@ -15,27 +15,10 @@ const handleListen = () => {
 }
 
 const server = http.createServer(app);
-const wss = new websocket.Server({ server });
+const webServer = socketIO(server);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonymous";
-    console.log("connected to Browser"); //브라우저와 연결 성공시 
-    socket.on("close", () => { console.log("Disconnected from the Browser") }); //브라우저 연결이 끊어지면 발생
-
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_Message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}:${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-
-
-});
+webServer.on("connection", (socket) => {
+    console.log(socket);
+})
 server.listen(3000, handleListen);
 
