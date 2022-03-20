490ca1780ee1740b9f1b3944c25157b2e516f018
Chi Hwan Kim <rlaclghks123@naver.com>
2022-03-20 18:33:39 +0900

    chat room 입장

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,11 +2,24 @@ const socket = io();
 
 const welcome = document.querySelector("#welcome");
 const form = welcome.querySelector("form");
+const room = document.querySelector("#room");
+
+let roomName;
+
+room.hidden = true;
+
+const showRoom = () => {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+}
 
 const handleRoomSubmit = (event) => {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, done = () => { console.log("server is done!") });
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -18,11 +18,12 @@ const server = http.createServer(app);
 const webServer = socketIO(server);
 
 webServer.on("connection", (socket) => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
-        setTimeout(() => {
-            done();
-        }, 10000);
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
     })
 
 });
